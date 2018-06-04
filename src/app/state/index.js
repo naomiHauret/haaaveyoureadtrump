eec20442598ab8f7285196e58265c257665dc8bf
naomiHauret <hauretnaomi@gmail.com>
2018-06-05 00:10:39 +0200

    reinitialize state

--- src/app/state/index.js
+++ src/app/state/index.js
@@ -5,6 +5,6 @@ export default {
   showHomeText: false,
   donaldMenuVisibleParagraph: null,
   unfoldBurger: false,
-  currentBurger: ["Fake News", "Obamacare", "MAGA", "Crooked Hilary"],
-  showGraph: true,
+  currentBurger: [],
+  showGraph: false,
 }
