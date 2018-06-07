f92952d425c1221ee0d0d26782b9b4ba25af77ed
naomiHauret <hauretnaomi@gmail.com>
2018-06-07 14:33:23 +0200

    :tada:

--- src/app/views/pages/DonaldMenu/index.js
+++ src/app/views/pages/DonaldMenu/index.js
@@ -71,16 +71,33 @@ export default () => (state, actions) => {
             </button>
           </div>
         )}
-        <Enter
-          time={650}
-          easing="ease-in-out"
-          css={{
-            transform: "translateY(-600px)",
-          }}
-        >
-          <div class={cxs({ zIndex: 2 })}>
+        <Enter time={650} easing="ease-in-out" css={{ transform: "translateY(-600px)" }}>
+          <div
+            class={cxs({
+              zIndex: 2,
+            })}
+          >
             <Heading level={1} brand={state.showGraph === true ? "light" : "dark"} margin={0}>
-              The<br />Donald's menu
+              {state.currentBurger.length < 0 ? (
+                <span>
+                  The <br /> Donald's menu
+                </span>
+              ) : (
+                <span
+                  class={cxs({
+                    "-webkit-text-fill-color": "transparent",
+                    "-webkit-background-clip": "text",
+                    backgroundImage: `linear-gradient( to right, ${ds.get("colors.texts.headings.light")}, ${ds.get(
+                      "colors.texts.headings.light",
+                    )} 50%, ${ds.get("colors.texts.headings.dark")} 50%)`,
+                    backgroundSize: "200%",
+                    backgroundPosition: state.showGraph === true ? "200%" : "100%",
+                    transition: "all 405ms ease-in-out",
+                  })}
+                >
+                  The <br /> Donald's menu
+                </span>
+              )}
             </Heading>
           </div>
         </Enter>
@@ -109,14 +126,7 @@ export default () => (state, actions) => {
               })}
             >
               {state.currentBurger.length > 0 && (
-                <Enter
-                  time={550}
-                  easing="ease-in-out"
-                  css={{
-                    transform: "translateY(50px) scale(1.25)",
-                    opacity: 0,
-                  }}
-                >
+                <Enter time={550} easing="ease-in-out" css={{ transform: "translateY(50px) scale(1.25)", opacity: 0 }}>
                   <div>
                     <Burger
                       key="selectsliceburger"
@@ -130,15 +140,7 @@ export default () => (state, actions) => {
                 </Enter>
               )}
               {state.currentBurger.length === 0 && (
-                <Enter
-                  time={450}
-                  delay={355}
-                  easing="ease-in-out"
-                  css={{
-                    transform: "translateY(20px)",
-                    opacity: 0,
-                  }}
-                >
+                <Enter time={450} delay={355} easing="ease-in-out" css={{ transform: "translateY(20px)", opacity: 0 }}>
                   <p>
                     Reading Donald Trump is a real<br />
                     feast! See how is composed the<br />
@@ -175,15 +177,7 @@ export default () => (state, actions) => {
             >
               {state.currentBurger.length > 0 &&
                 state.showGraph === false && (
-                  <Enter
-                    time={550}
-                    delay={65}
-                    easing="ease-in-out"
-                    css={{
-                      transform: "scale(0)",
-                      opacity: 0,
-                    }}
-                  >
+                  <Enter time={550} delay={65} easing="ease-in-out" css={{ transform: "scale(0)", opacity: 0 }}>
                     <div>
                       <Burger
                         key="customburger"
@@ -200,14 +194,7 @@ export default () => (state, actions) => {
                 )}
               {state.currentBurger.length === 0 &&
                 state.showGraph === false && (
-                  <Enter
-                    time={350}
-                    delay={525}
-                    easing="ease-in-out"
-                    css={{
-                      opacity: 0,
-                    }}
-                  >
+                  <Enter time={350} delay={525} easing="ease-in-out" css={{ opacity: 0 }}>
                     <Burger
                       key="helloburger"
                       unfoldBurger={state.unfoldBurger}
@@ -256,6 +243,7 @@ export default () => (state, actions) => {
                       state.showGraph === false
                         ? ds.get("colors.texts.legends.colorful")
                         : ds.get("colors.texts.legends.light"),
+                    transition: "all 405ms ease-in-out",
                   })}
                 >
                   <div
--- src/app/views/pages/Home/index.js
+++ src/app/views/pages/Home/index.js
@@ -15,8 +15,10 @@ export default () => (state, actions) => {
   const metaTitle = homeMetaTitle
   const key = "khome"
 
-  return <Page state={state} actions={actions} metaTitle={metaTitle} key={key}>
-      <div class={cxs({
+  return (
+    <Page state={state} actions={actions} metaTitle={metaTitle} key={key}>
+      <div
+        class={cxs({
           textAlign: "center",
           margin: "auto",
           display: "flex",
@@ -26,11 +28,15 @@ export default () => (state, actions) => {
           "@media (min-width: 992px)": {
             width: "50%",
           },
-        })}>
+        })}
+      >
         <Heading brand="light" level={1} margin="0 0 0 0">
-          <div class={cxs({
+          <div
+            class={cxs({
               fontSize: ds.pxTo(150, baseFontSize, "rem"),
-            })} oncreate={(e) => anime({
+            })}
+            oncreate={(e) =>
+              anime({
                 targets: e,
                 fontSize: [
                   ds.pxTo(150, baseFontSize, "rem"),
@@ -45,13 +51,17 @@ export default () => (state, actions) => {
                 delay: 3350,
                 easing: "easeInOutBack",
                 width: "80vw",
-              })}>
+              })
+            }
+          >
             Haaave you<br />read Trump ?
           </div>
         </Heading>
 
         <Heading brand="light" level={2} margin="0">
-          <div oncreate={(e) => anime({
+          <div
+            oncreate={(e) =>
+              anime({
                 targets: e,
                 opacity: [0, 1],
                 translateY: [-15, 0],
@@ -67,12 +77,17 @@ export default () => (state, actions) => {
                   duration: 550,
                 },
                 easing: "easeInOutSine",
-              })}>
+              })
+            }
+          >
             A deep analysis of the USA President’s tweets
           </div>
         </Heading>
 
-        <div class="anime-twitter" oncreate={(e) => anime({
+        <div
+          class="anime-twitter"
+          oncreate={(e) =>
+            anime({
               targets: e,
               opacity: [0, 1],
               scale: [0, 1],
@@ -89,18 +104,28 @@ export default () => (state, actions) => {
               time: 2050,
               delay: 4750,
               easing: "easeInOutBack",
-  })}>
-          <svg class={cxs({
+            })
+          }
+        >
+          <svg
+            class={cxs({
               width: "100%",
               height: "100%",
               objectFit: "cover",
               objectPosition: "center",
-            })} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 46.5 37.8">
-            <path d="M0 33.4c4.2 2.7 9.1 4.2 14.4 4.2 17.3 0 26.7-14.3 26.7-26.7V9.7c1.8-1.3 3.4-3 4.7-4.9-1.7.7-3.5 1.2-5.4 1.5 1.9-1.2 3.4-3 4.1-5.2-1.8 1.1-3.8 1.9-6 2.3-1.7-1.8-4.2-3-6.8-3-5.2 0-9.4 4.2-9.4 9.4 0 .7.1 1.4.2 2.1-7.8-.4-14.7-4.1-19.3-9.8-.8 1.4-1.3 3-1.3 4.7 0 3.3 1.7 6.1 4.2 7.8-1.5 0-3-.5-4.3-1.2v.1c0 4.5 3.2 8.3 7.5 9.2-.8.2-1.6.3-2.5.3-.6 0-1.2-.1-1.8-.2 1.2 3.7 4.7 6.4 8.8 6.5-3.2 2.5-7.3 4-11.7 4-.6.2-1.4.1-2.1.1z" fill="currentColor" />
+            })}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 46.5 37.8"
+          >
+            <path
+              d="M0 33.4c4.2 2.7 9.1 4.2 14.4 4.2 17.3 0 26.7-14.3 26.7-26.7V9.7c1.8-1.3 3.4-3 4.7-4.9-1.7.7-3.5 1.2-5.4 1.5 1.9-1.2 3.4-3 4.1-5.2-1.8 1.1-3.8 1.9-6 2.3-1.7-1.8-4.2-3-6.8-3-5.2 0-9.4 4.2-9.4 9.4 0 .7.1 1.4.2 2.1-7.8-.4-14.7-4.1-19.3-9.8-.8 1.4-1.3 3-1.3 4.7 0 3.3 1.7 6.1 4.2 7.8-1.5 0-3-.5-4.3-1.2v.1c0 4.5 3.2 8.3 7.5 9.2-.8.2-1.6.3-2.5.3-.6 0-1.2-.1-1.8-.2 1.2 3.7 4.7 6.4 8.8 6.5-3.2 2.5-7.3 4-11.7 4-.6.2-1.4.1-2.1.1z"
+              fill="currentColor"
+            />
           </svg>
         </div>
 
-        <div class={cxs({
+        <div
+          class={cxs({
             fontWeight: ds.get("typo.fontWeight.light"),
             fontFamily: ds.get("typo.fontFamily.headings.h2"),
             position: "relative",
@@ -130,7 +155,9 @@ export default () => (state, actions) => {
               minWidth: "2px",
               backgroundColor: "currentColor",
             },
-          })} oncreate={(e) => anime({
+          })}
+          oncreate={(e) =>
+            anime({
               targets: e,
               opacity: 1,
               height: "45vh",
@@ -139,20 +166,21 @@ export default () => (state, actions) => {
               margin: "11.71875% 0",
               delay: 7500,
               ease: "easeInOutCubic",
-              begin:  (anim)  => {
+              begin: (anim) => {
                 anime({
                   targets: ".anime-twitter",
                   scale: 0,
                   margin: 0,
                   height: 0,
                   ease: "easeInOutCubic",
-
                 })
-              }
-            })}>
+              },
+            })
+          }
+        >
           <p>
-            Besides being the new president of The United States and a very contraversal man, Donal Trump is also
-            known for his absolute passion: Twitter.
+            Besides being the new president of The United States and a very contraversal man, Donal Trump is also known
+            for his absolute passion: Twitter.
           </p>
           <p>
             With more than 37.4k messages since his subscription, this analysis only run from the 24th October 2016 to
@@ -160,18 +188,23 @@ export default () => (state, actions) => {
           </p>
           <p>This represent over 250 tweets.</p>
         </div>
-      <div oncreate={(e) => anime({
-          targets: e,
-          opacity: [0, 1],
-          height: {
-            value: [0, "auto"],
-          },
-          translateY: "-15px",
-          ease: "easeInOutCubic",
-          delay: 7500,
-        })}>
+        <div
+          oncreate={(e) =>
+            anime({
+              targets: e,
+              opacity: [0, 1],
+              height: {
+                value: [0, "auto"],
+              },
+              translateY: "-15px",
+              ease: "easeInOutCubic",
+              delay: 7500,
+            })
+          }
+        >
           <ScrollIcon next={aboutPageUrl} />
         </div>
       </div>
     </Page>
+  )
 }
